0b04560930b53391290c15b88594dcc48725895a
Rahul Khan <khanrahul.1011@gmail.com>
2026-02-21 22:56:37 +0530

    Added storyblokeditable to hero and page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { getStoryblokApi } from "../lib/storyblok";
 import { StoryblokServerComponent } from "@storyblok/react/rsc";
+import { draftMode } from "next/headers";
 
 const fetchHomePage = async () => {
+  const { isEnabled } = draftMode();
   const client = getStoryblokApi();
   const { data } = await client.get(`cdn/stories/home/`, {
-    version: process.env.NODE_ENV === "development" ? "draft" : "published",
+    version: process.env.NODE_ENV === "development" || isEnabled ? "draft" : "published",
     resolve_relations: "recommended_tours.tours",
   });
   return data.story;
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -3,7 +3,7 @@ import { StoryblokComponent } from '@storyblok/react/rsc';
 import { storyblokEditable, StoryblokServerComponent } from '@storyblok/react/rsc';
 
 const Page = ({ blok }) => (
-	<main>
+	<main {...storyblokEditable(blok)}>
 		{blok.block?.map((nestedBlok) => (
 			<StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
 		))}
