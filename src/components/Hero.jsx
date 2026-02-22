e3cb74da6296f71da702ddb5c1bd6732ba666358
Rahul Khan <khanrahul.1011@gmail.com>
2026-02-22 23:43:35 +0530

    Header css changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { storyblokEditable } from '@storyblok/react/rsc';
 
 const Hero = ({ blok }) => {
   return (
-    <div {...storyblokEditable(blok)} className="hero-main container mx-auto px-4 w-full pt-32 pb-16">
+    <div {...storyblokEditable(blok)} className="hero-main container mx-auto px-4 w-full pt-16 pb-16">
       <h1 className="text-center text-5xl md:text-7xl font-bold">{blok.headline}</h1>
       <p className="text-center text-xl mt-8">{blok.content}</p>
     </div>
