5974b86eee10d88d111fe886a21ee142fbb304ee
Rahul Khan <khanrahul.1011@gmail.com>
2026-02-23 22:59:22 +0530

    Tour details page css update

--- src/components/Tour.tsx
+++ src/components/Tour.tsx
@@ -3,7 +3,7 @@ import { storyblokEditable, StoryblokServerRichText } from '@storyblok/react/rsc
 
 const Tour = (props: any) => {
   return (
-    <main {...storyblokEditable(props.blok)} className="tour-page-main container mx-auto px-4 w-full pt-32 pb-16">
+    <main {...storyblokEditable(props.blok)} className="tour-page-main container mx-auto px-4 w-full pt-16 pb-16">
       <h1 className='text-3xl md:text-5xl font-bold'>
         {props.blok.name}
       </h1>
@@ -11,8 +11,13 @@ const Tour = (props: any) => {
       <p className='mt-12 text-lg md:text-2xl md:leading-relaxed'>
         {props.blok.introduction}
       </p>
-      <div className='prose md:prose-lg mt-16 max-w-none'>
-        <StoryblokServerRichText doc={props.blok.body} />
+      <div className='tour-body-main mt-12'>
+        <div className='image-container'>
+          <img className='' src={props.blok.secondary_image.filename} alt={props.blok.secondary_image?.alt || props.blok.name}/>
+        </div>
+        <div className='content-container'>
+          <StoryblokServerRichText doc={props.blok.body} />
+        </div>
       </div>
     </main>
   );
