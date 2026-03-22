0eb8a79f9d43c53cec2720d6027af6123d28b449
Rahul Khan <khanrahul.1011@gmail.com>
2026-03-23 00:06:41 +0530

    Added Video Content Popup block

--- src/components/RecommendedTour.jsx
+++ src/components/RecommendedTour.jsx
@@ -16,7 +16,7 @@ export const RecommendedTour = (props) => {
           </p>
         </div>
         <p className="text-white uppercase font-bold mt-2 text-sm tracking-wide">{props.story.content.location}, India</p>
-        <Link className="bg-green-400 px-4 py-2 text-sky-950 max-w-fit rounded-lg font-bold text-base mt-8 block hover:bg-green-100" href={`/${props.story.full_slug}`}>View Tour</Link>
+        <Link className="bg-green-100 px-4 py-2 text-sky-950 max-w-fit rounded-lg font-bold text-base mt-8 block hover:bg-green-400" href={`/${props.story.full_slug}`}>View Tour</Link>
       </div>
     </div>
   );
